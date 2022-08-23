c66a11866728761950bcf5ddeb6783327abd7c4f
SaltedFishy03 <math740v@elev.techcollege.dk>
2022-08-23 14:29:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+static void skrivVelkomst()
+{
+    Console.Clear();
+    Console.WriteLine("_____________________");
+    Console.WriteLine("Velkommen til min app");
+    Console.WriteLine("_____________________");
+
+}
+
+
+
+static int ReadInt(string prompt)
+{
+    Console.Write(prompt);
+    string input = Console.ReadLine();
+    int value = Convert.ToInt32(input);
+    return value;
+}
+
+
+
+ConsoleKey tast;
+do
+{
+    skrivVelkomst();
+    int alder = ReadInt("Indtast et tal: ");
+    int morAlder = ReadInt("indtast et andet tal: ");
+    Console.WriteLine("Differencen mellem dig og din mor er: " + (morAlder - alder) + " år.");
+    Console.WriteLine ("Tryk på en tast for at starte forfra. ESC for at afslutte...");
+    tast = Console.ReadKey().Key;
+} while (tast != ConsoleKey.Escape);
